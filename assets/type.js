7be2cdf9a078561c7f850412e159ced325f7aac2
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-04-04 18:05:00 -0500

    Added dynamic type

---
+++ assets/type.js
@@ -0,0 +1,46 @@
+var element = document.querySelector('#test');
+var words = ['Web applications.','User interfaces.','Landing pages.','Coorporate websites.'];
+
+function type(words, index){
+    index = index ? index : 0;
+    (function writer(i){
+        var string = words[index];
+        if(string.length <= i++){
+            element.innerText = string;
+            if(words[index] != words[words.length-1]) {
+                setTimeout(function() {
+                    reverseType(words, index);
+                },500);
+            }else{
+                setTimeout(function() {
+                    reverseType(words, index);
+                },2000);
+            }
+            return;
+        }
+        element.innerText = string.substring(0,i);
+        var rand = Math.floor(Math.random() * (100)) + 140;
+        setTimeout(function(){writer(i);},rand);
+    })(0)
+}
+
+function reverseType(words, index){
+    index = index ? index : 0;
+    (function writer(i){
+        var string = words[index];
+        if(string.length <= i++){
+            element.innerText = string;
+            if(words[index] != words[words.length-1]) {
+                type(words, index+1);
+            }else{
+                type(words, 0);
+            }
+            return;
+        }
+        element.innerText = string.substring(0,string.length - i);
+        var rand = Math.floor(Math.random() * (100)) + 140;
+        setTimeout(function(){writer(i);},rand);
+    })(0)
+}
+
+type(words);
